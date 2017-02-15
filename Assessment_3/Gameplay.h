c6e9c2b06128d96f6712168060d5a9b730d0d52f
aaron.andrews <aaron.andrews@SEA-8E413E>
2017-02-15 12:02:13 -0800

    Added Controllers and am integrating transforms

--- Assessment_3/Gameplay.h
+++ Assessment_3/Gameplay.h
@@ -5,49 +5,31 @@
 class GamePlay
 {
 	unsigned credits_page_background;
+	unsigned player_sprite;
 	APP_STATE state;
 	Factory factory;
+
+	void FillObjectPool();
 
 public:
 	void init();
 	void draw();
-	void step();
+	void step(float t);
 	APP_STATE next(APP_STATE state);
 };
 
-void FillObjectPool()
+void GamePlay::FillObjectPool()
 {
-	// Create the room
-
-	// Place the player
-}
-
-void TakeInputFromPlayer()
-{
-
-	if (sfw::getKey('W'))
-	{
-		
-	}
-	else if (sfw::getKey('S'))
-	{
-		
-	}
-
-	if (sfw::getKey('A'))
-	{
-
-	}
-	else if (sfw::getKey('D'))
-	{
-
-	}
+	// Create the player
+	factory.spawnPlayer(credits_page_background);
 }
 
 void GamePlay::init()
 {
 	state = GAMEPLAY;
 	credits_page_background = sfw::loadTextureMap("./Images/Game_Stage.png");
+	player_sprite = sfw::loadTextureMap("./Images/Back_Button.png");
+	factory.spawnPlayer(player_sprite);
 }
 
 void GamePlay::draw()
@@ -55,9 +37,26 @@ void GamePlay::draw()
 	sfw::drawTexture(credits_page_background, 800, 450, 1600, 900);
 }
 
-void GamePlay::step()
+void GamePlay::step(float t)
 {
+	//cout << "step \n";
+	for (auto e = factory.begin(); e != factory.end();)
+	{
+		//cout << "for loop \n";
+		if (e->sprite)
+		{
+			//cout << "draw sprite \n";
+			e->sprite->draw(*e->trans);
+		}
 
+		// Controller system
+		if (e->cntlr && e->trans)
+		{
+			(*(e->cntlr))->Update(*e->trans, t);
+		}
+
+		++e;
+	}
 }
 
 APP_STATE GamePlay::next(APP_STATE state)
