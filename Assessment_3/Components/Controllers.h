14dcfd384fba31ab150403a33ca86c5064732185
aaron.andrews <aaron.andrews@SEA-8E413E>
2017-03-14 17:55:27 -0700

    changes

--- Assessment_3/Components/Controllers.h
+++ Assessment_3/Components/Controllers.h
@@ -9,15 +9,19 @@ protected:
 	int m_health, m_damage;
 	bool isAttacking;
 public:
+	Controller * parentCntlr;
+	int m_creep_count;
 	bool isBeingDamaged, isWeapon, isPlayerAlly, isAlive, showDamageSprite;
-	bool creep_0_active, creep_1_active, creep_2_active;
-	bool creep_0_activate, creep_1_activate, creep_2_activate;
+	//bool creep_0_active, creep_1_active, creep_2_active;
+	//bool creep_0_activate, creep_1_activate, creep_2_activate;
+	bool creeps_activate, creeps_active;
 
 	Controller() {}
-	virtual int Update(RigidBody &rb, Transform &trans, unsigned sprite, float t) { cout << "Controller is updating instead of the correct controller!!!!\n"; return 0; }
+	virtual int Update(unsigned sprite, float t) { cout << "Controller is updating instead of the correct controller!!!!\n"; return 0; }
 	virtual void takeDamage(unsigned damage) {}
 	virtual unsigned getDamage() { return m_damage; }
 	virtual Vec2 getSpawnLocation(Vec2 boss_pos, Vec2 player_pos) { return Vec2(0, 0); }
+	virtual void TravelTo(RigidBody &rb, Transform &trans, Vec2 dest);
 };
 
 class ControllerPlayer : public Controller
@@ -26,38 +30,14 @@ public:
 	ControllerPlayer() : Controller() {m_speed = 200, m_health = 100, m_damage = 0, showDamageSprite = false, isBeingDamaged = false, isAttacking = false, 
 										isAlive = true, isWeapon = false, isPlayerAlly = true, attackTimer = 0, attackTimerLimit = 0.25f; }
 
-	int Update(RigidBody &rb, Transform &trans, unsigned weapon_sprite, float t)
+	int Update(unsigned weapon_sprite, float t)
 	{
 		int retVal = 0;
 
-		rb.setVelocity(Vec2(0, 0));
 		//cout << trans.m_position.x;
 		//cout << "ControllerPlayer Update\n";
 		if (!isAttacking)
 		{
-			if (sfw::getKey('W'))
-			{
-				//cout << "detecting W\n";
-				rb.setVelocity(Vec2(0, m_speed) + rb.getVelocity());
-				trans.m_facing = 90;
-			}
-			else if (sfw::getKey('S'))
-			{
-				rb.setVelocity(Vec2(0, -m_speed) + rb.getVelocity());
-				trans.m_facing = 270;
-			}
-
-			if (sfw::getKey('A'))
-			{
-				rb.setVelocity(Vec2(-m_speed, 0) + rb.getVelocity());
-				trans.m_facing = 180;
-			}
-			else if (sfw::getKey('D'))
-			{
-				rb.setVelocity(Vec2(m_speed, 0) + rb.getVelocity());
-				trans.m_facing = 0;
-			}
-
 			if (sfw::getKey('L'))
 			{
 				// Spawn sword and attack
@@ -90,6 +70,37 @@ public:
 	}
 
 	unsigned getDamage() { return m_damage; }
+
+	void TravelTo(RigidBody &rb, Transform &trans, Vec2 unused_v)
+	{
+		rb.setVelocity(Vec2(0, 0));
+
+		if (!isAttacking)
+		{
+			if (sfw::getKey('W'))
+			{
+				//cout << "detecting W\n";
+				rb.setVelocity(Vec2(0, m_speed) + rb.getVelocity());
+				trans.m_facing = 90;
+			}
+			else if (sfw::getKey('S'))
+			{
+				rb.setVelocity(Vec2(0, -m_speed) + rb.getVelocity());
+				trans.m_facing = 270;
+			}
+
+			if (sfw::getKey('A'))
+			{
+				rb.setVelocity(Vec2(-m_speed, 0) + rb.getVelocity());
+				trans.m_facing = 180;
+			}
+			else if (sfw::getKey('D'))
+			{
+				rb.setVelocity(Vec2(m_speed, 0) + rb.getVelocity());
+				trans.m_facing = 0;
+			}
+		}
+	}
 };
 
 class ControllerPlayerWeapon : public Controller
@@ -125,6 +136,8 @@ public:
 	}
 
 	unsigned getDamage() { return m_damage; }
+
+	void TravelTo(RigidBody &rb, Transform &trans, Vec2 unused_v) {}
 };
 
 
@@ -177,41 +190,66 @@ public:
 			if (m_health <= 0)
 			{
 				isAlive = false;
+				parentCntlr->m_creep_count -= 1;
 			}
 		}
 	}
 
 	unsigned getDamage() { return m_damage; }
+
+	void TravelTo(RigidBody &rb, Transform &trans, Vec2 dest)
+	{
+		rb.setVelocity(Vec2(0, 0));
+
+		if (!isAttacking)
+		{
+			Vec2 dir = dest - trans.m_position;
+			dir = normal(dir);
+
+			rb.setVelocity(dir*m_speed + rb.getVelocity());
+		}
+	}
 };
 
 class ControllerBoss : public Controller
 {
+private:
+	bool collectingDead, exitLeft, exitRight;
+	float distanceAlongCurve;
+	Vec2 startPoint, middlePoint, endPoint;
+
 public:
 	ControllerBoss() { showDamageSprite = false, isAlive = true, isBeingDamaged = false, isAttacking = false, isWeapon = false, isPlayerAlly = false,
 						m_damage = 0, m_speed = 400, m_health = 100, damageTimer = 0, damageTimerLimit = 0.3,
-						creep_0_active = false, creep_1_active = false, creep_2_active = false, creep_0_activate = false, creep_1_activate = false, creep_2_activate = false;}
+						creeps_active = false, creeps_activate = false;}
 
 	int Update(RigidBody &rb, Transform &trans, unsigned sprite, float t)
 	{
-		if (trans.m_position.x >= 1700)
+		//if (trans.m_position.x >= 1700)
+		//{
+		//	rb.setVelocity(Vec2(-m_speed, 0));
+		//}
+		//else if (trans.m_position.x <= 000)
+		//{
+		//	rb.setVelocity(Vec2(m_speed, 0));
+		//}
+		//else if (trans.m_position.x > 400 && !creep_0_active)
+		//{
+		//	creep_0_activate = true;
+		//}
+		//else if (trans.m_position.x > 700 && !creep_1_active)
+		//{
+		//	creep_1_activate = true;
+		//}
+		//else if (trans.m_position.x > 1000 && !creep_2_active)
+		//{
+		//	creep_2_activate = true;
+		//}
+
+		distanceAlongCurve += m_speed * t;
+		if (distanceAlongCurve > 1)
 		{
-			rb.setVelocity(Vec2(-m_speed, 0));
-		}
-		else if (trans.m_position.x <= 000)
-		{
-			rb.setVelocity(Vec2(m_speed, 0));
-		}
-		else if (trans.m_position.x > 400 && !creep_0_active)
-		{
-			creep_0_activate = true;
-		}
-		else if (trans.m_position.x > 700 && !creep_1_active)
-		{
-			creep_1_activate = true;
-		}
-		else if (trans.m_position.x > 1000 && !creep_2_active)
-		{
-			creep_2_activate = true;
+			distanceAlongCurve = 0;
 		}
 
 		if (isBeingDamaged)
@@ -233,7 +271,6 @@ public:
 					showDamageSprite = true;
 			}
 		}
-
 		return 0;
 	}
 
@@ -298,5 +335,17 @@ public:
 			return boss_pos + Vec2(-200, 0);
 		if (right)
 			return boss_pos + Vec2(200, 0);
+	}
+
+	void TravelTo(RigidBody &rb, Transform &trans, Vec2 dest)
+	{
+		trans.m_position = quadBezier(startPoint, endPoint, middlePoint, distanceAlongCurve);
+	}
+
+	void GetNewBezier(Vec2 end)
+	{
+		startPoint  = endPoint;
+		endPoint    = end;
+		middlePoint = Vec2(rand() % abs((int)(startPoint.y - endPoint.y)), rand() % abs((int)(startPoint.x - endPoint.x)));
 	}
 };
